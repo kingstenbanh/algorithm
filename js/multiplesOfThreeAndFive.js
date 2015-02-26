0bef1ca487c89ec1a1625a1588b9a01fd72d6928
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-25 21:13:57 -0700

    update solution to constant time complexity

--- js/multiplesOfThreeAndFive.js
+++ js/multiplesOfThreeAndFive.js
@@ -2,6 +2,7 @@
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
  */
+ // O(n) time complexity solution
  var multiplesOfThreeAndFive = function(input) {
   var sum = 0;
 
@@ -12,5 +13,15 @@
   }
   return sum;
 }
-
 console.log(multiplesOfThreeAndFive(1000));
+
+// O(1) time complexity solution
+function sumDivisibleBy(target, n) {
+  p = Math.floor(target / n);
+  return n * (p * (p + 1)) / 2;
+}
+
+var multplesOf3And5 = function(target) {
+  return sumDivisibleBy(target, 3) + sumDivisibleBy(target, 5) - sumDivisibleBy(target, 15);
+}
+console.log(multplesOf3And5(1000));
--- swift/multiplesOfThreeAndFive.playground/section-1.swift
+++ swift/multiplesOfThreeAndFive.playground/section-1.swift
@@ -1,13 +1,24 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
-func multiplesOfThreeAndFive(input: Int) {
+import Darwin
+
+func multiplesOfThreeAndFive(input: Int) -> Int {
     var sum = 0
     for var i = 0; i < input; i++ {
         if i % 3 == 0 || i % 5 == 0 {
             sum += i
         }
     }
-    println(sum)
+    return sum
 }
 
 multiplesOfThreeAndFive(1000)
 
+func sumDivisibleBy(target: Int, n: Int) -> Int {
+    let p = (target - 1) / n
+    return n * (p * (p + 1)) / 2
+}
+
+func multiplesOf3And5(target: Int) -> Int {
+    return sumDivisibleBy(target, 3) + sumDivisibleBy(target, 5) - sumDivisibleBy(target, 15)
+}
+multiplesOf3And5(1000)
